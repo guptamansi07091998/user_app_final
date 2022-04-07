5312b18678380483f6298353a59f2a725bef2bee
Mansi Gupta <mansi.gupta1@indiamart.com>
2022-04-07 21:28:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigation/splash.kt
@@ -0,0 +1,41 @@
+package com.example.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.os.HandlerCompat.postDelayed
+import com.example.navigation.databinding.ActivitySplashBinding
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(
+
+                {
+                    val intent=Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+            },500
+        )
+    }
+
+
+
+}
